efa1748ffaa39c8323f2c72bdc6e1cf0b04e240d
Meherab Hassan <hassan47787@gmail.com>
2024-01-10 15:11:49 +0600

    first commit

---
+++ netflix/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='m-5'>
+      <Button>
+        Hellow from shadcnui
+      </Button>
+      <h1>The Button is Here</h1>
+    </div>
+  )
+}
